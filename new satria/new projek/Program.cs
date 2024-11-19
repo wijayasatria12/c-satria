fe6374d839ede0d191132a3d450749624a539360
wijayasatria12 <ars4t25@gmail.com>
2024-11-19 13:32:04 +0700

    Add files via upload

---
+++ new satria/new projek/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+class Program
+{
+ static void Main()
+ {
+ // Minta pengguna memasukkan nilai alas
+ Console.Write("Masukkan nilai alas jajargenjang: ");
+ double alas = Convert.ToDouble(Console.ReadLine());
+ // Minta pengguna memasukkan nilai tinggi
+ Console.Write("Masukkan nilai tinggi jajargenjang: ");
+ double tinggi = Convert.ToDouble(Console.ReadLine());
+ // Hitung luas segitiga
+ double luas = 0.5 * alas * tinggi;
+ // Tampilkan hasil luas segitiga
+ Console.WriteLine("Luas jajargenjang adalah: " + luas);
+ }
+}
